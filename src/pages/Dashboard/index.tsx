8784734ac88b4bc64a0bdf5333b605f9ea0184d3
willfeliz1 <william_mfelizardo@hotmail.com>
2020-08-07 18:29:39 -0300

    feat: redirect and load FoodDetail when food is select

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -55,6 +55,7 @@ const Dashboard: React.FC = () => {
 
   async function handleNavigate(id: number): Promise<void> {
     // Navigate do ProductDetails page
+    navigation.navigate('FoodDetails', { id });
   }
 
   useEffect(() => {
