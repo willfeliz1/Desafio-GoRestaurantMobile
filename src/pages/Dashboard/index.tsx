42d47d2231b12e75c597fa1fb9e11f7bf4405785
willfeliz1 <william_mfelizardo@hotmail.com>
2020-08-07 16:42:46 -0300

    feat: load foods and categories

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -60,6 +60,9 @@ const Dashboard: React.FC = () => {
   useEffect(() => {
     async function loadFoods(): Promise<void> {
       // Load Foods from API
+      api.get('foods').then(response => {
+        setFoods(response.data);
+      });
     }
 
     loadFoods();
@@ -68,6 +71,9 @@ const Dashboard: React.FC = () => {
   useEffect(() => {
     async function loadCategories(): Promise<void> {
       // Load categories from API
+      api.get('categories').then(response => {
+        setCategories(response.data);
+      });
     }
 
     loadCategories();
