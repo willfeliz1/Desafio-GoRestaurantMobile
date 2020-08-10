1ca7861726d8707b68276bb713a6ddb9490567bc
willfeliz1 <william_mfelizardo@hotmail.com>
2020-08-10 13:50:34 -0300

    feat: list favorite foods

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -33,6 +33,9 @@ const Favorites: React.FC = () => {
   useEffect(() => {
     async function loadFavorites(): Promise<void> {
       // Load favorite foods from api
+      api.get('favorites').then(response => {
+        setFavorites(response.data);
+      });
     }
 
     loadFavorites();
