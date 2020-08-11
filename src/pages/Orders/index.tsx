fe98b4bbbb3dc079a52162832dd29f7eac86ea3f
willfeliz1 <william_mfelizardo@hotmail.com>
2020-08-10 21:34:53 -0300

    feat: list orders

--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -33,6 +33,9 @@ const Orders: React.FC = () => {
   useEffect(() => {
     async function loadOrders(): Promise<void> {
       // Load orders from API
+      api.get('orders').then(response => {
+        setOrders(response.data);
+      });
     }
 
     loadOrders();
@@ -59,7 +62,7 @@ const Orders: React.FC = () => {
               <FoodContent>
                 <FoodTitle>{item.name}</FoodTitle>
                 <FoodDescription>{item.description}</FoodDescription>
-                <FoodPricing>{item.formattedPrice}</FoodPricing>
+                <FoodPricing>{formatValue(item.formattedValue)}</FoodPricing>
               </FoodContent>
             </Food>
           )}
