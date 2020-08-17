068e37c9c313c0a16b037e4ec21d7f89986a9030
willfeliz1 <william_mfelizardo@hotmail.com>
2020-08-17 20:30:41 -0300

    feat: Modal component with react-native-modal lib

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
+import ReactNativeModal from 'react-native-modal';
+
+interface IModalProps {
+  children: any;
+  isOpen: boolean;
+  ResetToHome: () => boolean;
+}
+
+const Modal: React.FC<IModalProps> = ({ children, isOpen, ResetToHome }) => {
+  const [modalStatus, setModalStatus] = useState(isOpen);
+
+  useEffect(() => {
+    setModalStatus(isOpen);
+  }, [modalStatus, isOpen]);
+
+  return (
+    <ReactNativeModal
+      onBackdropPress={ResetToHome}
+      isVisible={modalStatus}
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      {children}
+    </ReactNativeModal>
+  );
+};
+
+export default Modal;
